a06b2914de8f155f9d990348560df8b26603f7ae
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-29 08:37:02 -0700

    united states of poke react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import Card from 'antd/lib/card';
+import { Input } from 'antd';
 import './App.css';
 import pokemonMetadata from 'pokemon-metadata';
 
+const Search = Input.Search;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+    };
+  }
   render() {
     const pokemonNames = Object.keys(pokemonMetadata);
+    console.log(this.state.searchTerm)
     return (
+      <div className = "app">
+        <Search
+          placeholder="input search text"
+          style={{ width: 200 }}
+          onChange={e => this.setState({searchTerm: e.target.value})}
+        />
       <div className = "Container">
-        {pokemonNames.map((name) => {
-          const pokemon = pokemonMetadata[name]
-          return (
-            <PokemonCard
-              name={pokemon.name}
-              id={pokemon.id}
-              imgSource={pokemon.sprites.front_default}
-            />
-          )
-        })}
+          {pokemonNames.map((name) => {
+            const pokemon = pokemonMetadata[name]
+            return (
+              <PokemonCard
+                name={pokemon.name}
+                id={pokemon.id}
+                imgSource={pokemon.sprites.front_default}
+              />
+            )
+          })}
+        </div>
       </div>
     );
   }
